cd1868a293c2deb58e2f8101b7fb24f100b68147
umais <umais20@yahoo.com>
2015-04-27 01:25:33 -0400

    updated repo

---
+++ WebDevelopmentCourse/Controllers/ModulesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDevelopmentCourse.Controllers
+{
+    public class ModulesController : Controller
+    {
+        //
+        // GET: /Modules/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Module2()
+        {
+            return View();
+        }
+	}
+}
---
+++ WebDevelopmentCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDevelopmentCourse.Startup))]
+namespace WebDevelopmentCourse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
